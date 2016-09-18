e46ee3d4aed8b94951f4a1aa409ab51c52106f0a
ioef <iosifidise@gmail.com>
2016-09-18 14:25:38 +0300

    minor updates

--- Module2/subprocess_shell.py
+++ Module2/subprocess_shell.py
@@ -2,6 +2,8 @@
 
 from subprocess import call
 
+#The main problem of the below python script is that it allows the skip any sanity checks in the input of data. 
+#Therefore it would allow an attacker to input arbritary and potentially malicious data like SQL Injection.
 #This is like handing a loaded submachine gun to a child. For a example on malicious user like Trudy could issue
 #as a filename not_existent_filename; rm -rf *" 
 #Or not_existent; cat /etc/passwd
