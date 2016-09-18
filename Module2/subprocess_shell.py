1db3051b5ce4079ec7b8f3ac20482a30b8aa1c95
ioef <iosifidise@gmail.com>
2016-09-18 14:23:25 +0300

    minor updates

--- Module2/subprocess_shell.py
+++ Module2/subprocess_shell.py
@@ -2,10 +2,10 @@
 
 from subprocess import call
 
-#This is like handing a loaded submachine gun to a child
-#for a example on malicious user like Trudy could issue
-#as a filename not_existent_filename; rm -rf *"
-#or not_existent; cat /etc/passwd
+#This is like handing a loaded submachine gun to a child. For a example on malicious user like Trudy could issue
+#as a filename not_existent_filename; rm -rf *" 
+#Or not_existent; cat /etc/passwd
+#
 #Use it at your own risk! 
 filename = raw_input("Please provide a filename you want to view:")
 
