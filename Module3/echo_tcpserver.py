855f5230a7d40bfaef3df02f3a9318c8e9a7d0a6
ioef <iosifidise@gmail.com>
2016-09-18 16:17:39 +0300

    minor updates

---
+++ Module3/echo_tcpserver.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import socket
+
+
+#create a TCP socket
+tcpSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+bind_ip   = "0.0.0.0" 
+bind_port = 9999
+
+tcpSocket.bind((bind_ip,bind_port))
+tcpSocket.listen(2)
+
+print "Waiting for Client..."
+
+
+connection, (ip, sock) = tcpSocket.accept()
+
+print "Received connection from Client with IP: %s:%d" %(ip,sock)
+
+a = connection.recv(1024)
+print ("Received from the Client the message: %s" %a)
+
+clientEcho = "ECHO: "+ a
+
+connection.send(clientEcho)
+
+connection.close()
