05f93cfb5ac971a0b399b42c2972117a2d4e3bd5
ioef <iosifidise@gmail.com>
2016-09-18 12:39:22 +0300

    tcp server improved

--- Module2/tcp_server.py
+++ Module2/tcp_server.py
@@ -1,17 +1,54 @@
 #!/usr/bin/env python
 
+import sys
 import signal
+import socket 
 
-timer = 2
+
+#set the timer to 60 seconds as default 
+timer = 2 
 
 
 def alarm_handler(signum, frame):
 	exit(0)
 
-signal.signal(signal.SIGALRM, alarm_handler)
-signal.alarm(timer)
 
-while True:
-	pass
 
+def main():
+	
+	bind_ip="0.0.0.0"
+	bind_port=9999
+
+	#set the timer to 60 seconds as default 
+	timer = 2
+
+	#create the server socket
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.bind((bind_ip, bind_port))
+	server.listen(1)
+	
+	if len(sys.argv) == 2:
+		timer = int(sys.argv[1])
+
+	print "[*] Listening on %s:%d for %d seconds" %(bind_ip, bind_port, timer)
+	
+	if len(sys.argv) > 1  and sys.argv[1] == int:
+		timer = sys.argv[1]		
+	
+	#start the SIGALRM timer in order to terminate the server automatically 
+	signal.signal(signal.SIGALRM, alarm_handler)
+	signal.alarm(timer)
+
+
+	#server forever or until the alarm goes off :)
+	while True:
+		#Accept connections
+		client, addr = server.accept()
+
+	
+
+
+
+if __name__ == '__main__':
+	main()
 
