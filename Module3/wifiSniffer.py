3786999c2b4a808fa10e37e9ad63eaf83d6d4026
ioef <iosifidise@gmail.com>
2016-09-30 22:56:40 +0300

    Scapy HTTP Sniffer

--- Module3/wifiSniffer.py
+++ Module3/wifiSniffer.py
@@ -4,6 +4,16 @@
 from scapy.all import *
 
 
-packets = sniff(iface = "wlan0")
+def wificatcher(packets):
+	for packet in packets:
+		#check if Packet is a Beacon 
+		if packet.haslayer(Dot11Beacon):
+			if (packet[Dot11].addr3) and packet[Dot11].addr3 not in wifiList:
+				wifiList.append(packet[Dot11].addr3)
+				print "[+] Found SSID: %s with MAC:%s" %(packet[Dot11].info,packet[Dot11].addr3)
+wifiList = []
+
+sniff(iface = "mon0", prn=wificatcher)
+
 
 
