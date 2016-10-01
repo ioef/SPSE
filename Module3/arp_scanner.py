a48560010c4805e58d59cea25f07f9575db37c04
ioef <iosifidise@gmail.com>
2016-10-01 21:07:46 +0300

    Scapy ARP Scanner

--- Module3/arp_scanner.py
+++ Module3/arp_scanner.py
@@ -17,11 +17,28 @@ signal(SIGINT,ctrlc_handler)
 
 def main():
 	signal(SIGINT,ctrlc_handler)
-	
+
+
+	#code that finds the IP subnet automatically
+	ip_sub = str(conf.route)
+	ip_sub = ip_sub.split('\n')
+
+	ip =""
+	for line in ip_sub:
+		if ("eth0" in line) or ("eth1" in line):
+			ip = line.split()[0]
+			ip = ip.split(".")
+			ip_prefix = ip[0]+'.' + ip[1]+'.' + ip[2] + '.'
+
+	if ip=="":
+		sys.exit()
+		
+	#end of code that finds the subnet automatically 
+
 	print "\nARP Scanner - Scanning for Devices"
 	print "======================================"
 	for ip_suffix in range(1,255):
-		ip = '192.168.2.' + str(ip_suffix)
+		ip = ip_prefix + str(ip_suffix)
 	
 		arpRequest = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip, hwdst="ff:ff:ff:ff:ff:ff")
 		#wait for 1 second before timeout and suppress the emission messages with verbose=0
