6d61a987781abb9dc7ea93bd08fad3bf6ad69062
ioef <iosifidise@gmail.com>
2016-10-01 15:02:48 +0300

    Scapy ARP Scanner

--- Module3/arp_subscanner.py
+++ Module3/arp_subscanner.py
@@ -13,11 +13,23 @@ def ctrlc_handler(signum,frame):
 
 signal(SIGINT,ctrlc_handler)
 
-for ip_suffix in range(1,255):
-	ip = '192.168.2.' + str(ip_suffix)
-	
-	arpRequest = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip, hwdst="ff:ff:ff:ff:ff:ff")
-	arpResponse = srp1(arpRequest, timeout=1, verbose=0)
 
-	if arpResponse:
-		print ("IP: %s MAC: %s" %(arpResponse.psrc,arpResponse.hwsrc))
+
+def main():
+	signal(SIGINT,ctrlc_handler)
+	
+	print "\nARP Scanner - Scanning for Devices"
+	print "======================================"
+	for ip_suffix in range(1,255):
+		ip = '192.168.2.' + str(ip_suffix)
+	
+		arpRequest = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip, hwdst="ff:ff:ff:ff:ff:ff")
+		#wait for 1 second before timeout and suppress the emission messages with verbose=0
+		arpResponse = srp1(arpRequest, timeout=1, verbose=0)
+
+		if arpResponse:
+			print ("IP: %s MAC: %s" %(arpResponse.psrc,arpResponse.hwsrc))
+
+
+if __name__=="__main__":
+	main()
