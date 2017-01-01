23e3b420ae831b39007838122a9574a33e1024d2
ioef <iosifidise@gmail.com>
2017-01-01 11:15:07 +0200

    ftp crawler

--- Module2/ftpclient.py
+++ Module2/ftpclient.py
@@ -1,44 +1,83 @@
 #!/usr/bin/env python
 
-import socket
-import sys
-from ftplib import FTP 
+import threading
+import Queue
+import time 
+from ftplib import FTP
 
 TIMEOUT = 1
 
-def ftpIPs():
-	ipList = []
-	with open('ftpsites.txt','r') as ftpsites:
-		for line in ftpsites:
-			ipList.append(line.strip('\n'))
+class WorkerThread(threading.Thread):
+	
+	def __init__(self, queue,lock):
+		threading.Thread.__init__(self)
+		self.queue = queue
+		self.lock = lock
 
-	return ipList
+	def run(self):
+		#print "In WorkerThread"
+	
+		step = 0
+		while True:
+			
+			self.lock.acquire()
+			#retrieve from the queue a single FTP Server IP
+			ftpServerIP = self.queue.get()
+			
+			print "\n [+] Attempting to connect to ftp server with IP: %s!" %ftpServerIP
+			ftp = FTP()
+			
+			try:
+
+				ftp.connect(ftpServerIP, 21, TIMEOUT)
+			
+				if ftp == None:  	
+					continue 		
+				ftp.login()
+				ftp.retrlines('LIST')
+				ftp.close()
+			except: 
+				print "Permission Denied or other error occured!"
+
+			print "[+] Finished fetching data from FTP Server %s "%ftpServerIP
+			time.sleep(1)
+			self.queue.task_done()
+			self.lock.release()
+
 
 def main():
 
+	noOfThreads = 22
+
+	with open("ftpsites.txt","r") as f:
+		ftpSites = f.readlines()
+
+
 	
-	serverIPList = ftpIPs()
+	#queue to fetch work from 
+	queue = Queue.Queue()
 	
-	for serverIP in serverIPList:
-		clientBanner = '\n[+] Attempting to connect to ftp server with IP: '  + serverIP + '\n'
-		sys.stdout.write(clientBanner)
-		ftp = FTP()
+	#create locks for the threads
+	lock=threading.Lock()
 
-		if ftp == None:
-			continue		
+	for i in range(noOfThreads):
+		#print "Creating WorkerThread : %d" %i
 
-		try:
-			ftp.connect(serverIP,21,TIMEOUT)	
-			ftp.login()
+		#feed the queue to the Workerthread in order the threads know on what they 'll be working on
+		worker = WorkerThread(queue,lock)
+		worker.setDaemon(True)
+		worker.start()
+		#print "WorkerThread %d Created!"%i
 
-			sys.stdout.write(ftp.getwelcome())
-			sys.stdout.write(ftp.retrlines('LIST'))
-			ftp.close()
-			sys.stdout.write('\n')
- 
-		except: 
-			continue 
+	
+	#create a queue which includes all the ftpsites in the file 
+	for ip in ftpSites:
+		queue.put(ip)
 
-if __name__ == "__main__":
+	queue.join()
+	print "All FTP sites scanned!"
+
+
+
+if __name__=="__main__":
 	main()
-
