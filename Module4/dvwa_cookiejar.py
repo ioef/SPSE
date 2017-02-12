4a9ea9d1c3342df7f4f9836dea3ef403b74dbfc6
ioef <iosifidise@gmail.com>
2017-02-12 15:14:42 +0200

    CookieJar Example

--- Module4/dvwa_cookiejar.py
+++ Module4/dvwa_cookiejar.py
@@ -5,19 +5,44 @@
 # credentials provided initally can be reused by a subsequent session
 
 import mechanize
-import os
+import sys
 from optparse import OptionParser
 
 
 
 def urlOpen():
+	br.
 	pass
 
 
 def main():
-	parser = OptionParser()
+	parser = OptionParser(usage="usage: %prog url [options]")
+	
+	parser.add_option('-u','--username',dest='username', help='Username for the login')
+	parser.add_option('-p','--password',dest='password', help='Password for the login')
+		
+	(options, args) = parser.parse_args()
 
+	if len(args) !=1:
+		parser.error("Incorrect number of arguments provided!")
+		#parser.print_help()
+		sys.exit(1)
 
+	hostname = args[0]
+
+	username = options.username 
+	password = options.password
+
+	if not username:
+		parser.error("No username provided")
+		sys.exit(1)
+
+	if not password:
+		parser.error("No password provided")
+		sys.exit(1)
+
+	#Create a CookieJar	
+	cookiejar = mech.CookieJar()
 
 if __name__ == "__main__":
 	main()
