bec8c813b500519b5e9dc388215e7b61dbfd327e
ioef <iosifidise@gmail.com>
2017-02-12 14:40:10 +0200

    CookieJar Example

---
+++ Module4/dvwa_cookiejar.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+# The CookieJar can be used in mechanize is order to create a list of Cookies.
+# Usefulness: The Cookie list can be inherited from a mechanize Session to another.Therefore the 
+# credentials provided initally can be reused by a subsequent session
+
+import mechanize
+import os
+from optparse import OptionParser
+
+
+
+def urlOpen():
+
+
+
+def main():
+	parser = OptionParser()
+
+
+
+if __name_ == "__main__":
+	main()
+
