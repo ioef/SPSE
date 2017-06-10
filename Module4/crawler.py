8b68b2fe56c573ea511c3e65cb0f0ac10e7f986e
ioef <iosifidise@gmail.com>
2017-06-10 20:05:48 +0300

    Threaded crawler

--- Module4/crawler.py
+++ Module4/crawler.py
@@ -19,44 +19,90 @@ from Queue import Queue
 from optparse import OptionParser
 from bs4 import BeautifulSoup
 import urllib2
+import sys
 #import MySQLdb
 
-
-
-MAX_DEPTH = 1 
 
 
 #create the class that will handle the MySQL Object
 #This class handles the mysql connection
 class MySQLDB:
-    def __init__(self, host, db, port, user, passwd):
-        self.hostname = host
+    #Class that is responsible for the connection data of the mysql
+    def __init__(self, hostname, port, db, user, passwd):
+        self.hostname = hostname
         self.port     = port
-        self.df       = db
+        self.db       = db
         self.user     = user
         self.passwd   = passwd
 
 
 class crawlEngine(threading.Thread):
-      def __init__(self, queue, base_url, max_depth, mysqlConnection):
+      def __init__(self, queue, base_url, max_depth, url_list, lock, mysqlConnection):
           threading.Thread.__init__(self)
           self.queue           = queue 
           self.base_url        = base_url
+          self.base_domain     = self.getdomain(base_url)
+          self.depth           = 0
+          self.url_list        = url_list
           self.max_depth       = max_depth
           self.mysqlConnection = mysqlConnection
-
+          self.lock            = lock
 
       def run(self):
           while True:
-              pass
+              url, self.depth = self.queue.get()
+              current_domain  = self.getdomain(url)
 
-      def openurl(self):
-          req = urllib2.Request(self.base_url)
-          req.add_header('User-agent','Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20130406 Firefox/23.0')
-          resp = urllib2.urlopen(req)
-          html = resp.read()
-        
-          return html  
+              if self.base_domain != current_domain:
+                  self.queue.task_done()
+                  continue
+              
+
+              html = self.openurl(url)
+              
+              if html !=-1:
+                  print "\r{0}Domain: {1} : depth: {2}".format('----'*self.depth, url, self.depth)
+                  if self.depth < self.max_depth:
+                      links = self.get_links(html)
+                      for link in links:
+                          self.addurltoList(link)
+                  else:
+                      self.queue.task_done()
+                      continue
+
+
+              self.queue.task_done()
+
+      def openurl(self, url):
+          try:
+              req = urllib2.Request(url)
+              req.add_header('User-agent','Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20130406 Firefox/23.0')
+              resp = urllib2.urlopen(req)
+              if resp.code == 200:
+                  html = resp.read()
+                  return html  
+              else:
+                  return -1
+          except:
+              return -1
+
+
+      def addurltoList(self, link):
+          
+          self.lock.acquire()
+          if link not in self.url_list:
+              self.url_list.append(link)
+              self.queue.put((link, self.depth +1))
+          self.lock.release()
+ 
+      def getdomain(self, url):
+          #extract the network location part e.g. the domain
+          if url.startswith('http') or url.startswith('https'):
+              domain = url.split('/')[2]
+          else:
+              domain = url.split('/')[0]
+
+          return domain
 
       def get_links(self,html):
    
@@ -80,17 +126,29 @@ class crawlEngine(threading.Thread):
 
 
 def main():
-    url = "https://www.google.com"
 
-    crawler = crawlEngine(1, url, 0, 0)
+    if len(sys.argv) !=2:
+        sys.exit(-1)
     
-    linkList = []
-    
-    html = crawler.openurl()
-    linkList = crawler.get_links(html) 
-    
-    for link in linkList:
-        print link
+    url = sys.argv[1]
+    threads = 5
+
+    #initialization of an empty Queue
+    queue = Queue()
+   
+    queue.put((url,1))
+
+    url_list = [] 
+    max_depth = 5
+
+    lock = threading.Lock()
+
+    for i in xrange(threads):
+        crawler = crawlEngine(queue, url, max_depth, url_list, lock, 0)
+        crawler.setDaemon(True)
+        crawler.start()
+
+    queue.join()
 
 
 if __name__ == "__main__":
