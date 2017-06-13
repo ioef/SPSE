0dc8dfc508a071930d8cf44fb7d370cd2939690a
ioef <iosifidise@gmail.com>
2017-06-13 22:03:53 +0300

    OWASP Top10 A6 added

--- Module4/OWASPTop10/a6.py
+++ Module4/OWASPTop10/a6.py
@@ -10,22 +10,42 @@ dirs = ['documentation', 'images']
 
 
 
-browser = mechanize.Browser()
+def dirlist():
+    browser = mechanize.Browser()
 
-for directory in dirs:
+    for directory in dirs:
     
-    request_url = url + directory
+        request_url = url + directory
 
-    print '[!] Currently Testing {0} \n'.format(request_url)
-    response = browser.open(request_url)
+        print '[!] Currently Testing {0} \n'.format(request_url)
+        response = browser.open(request_url)
+
+        page = response.read()
+
+        if 'Index of' in page:
+            print '[+] The web server allows directory listing!'
+            break
+
+
+
+
+def getforPOST():
+    browser = mechanize.Browser()
+
+    url = 'http://192.168.1.10/mutillidae/index.php?page=user-info.php&username=jeremy&password=password&user-info-php-submit-button=View+Account+Details'
+
+    response = browser.open(url)
 
     page = response.read()
 
-    if 'Index of' in page:
-        print '[+] The web server allows directory listing!'
-        break
+    if 'Results' in page:
+        print '[+] The server supports GET for POST and hence is vulnerable'
 
 
 
+def main():
+    dirlist()
+    getforPOST()
 
-
+if __name__=='__main__':
+    main()
