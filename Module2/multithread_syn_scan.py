fab07b00e8165fcc685817cd705d7bd7bbe1b32f
ioef <iosifidise@gmail.com>
2019-03-10 15:46:03 +0200

    Multithreaded example with second queue

--- Module2/multithread_syn_scan.py
+++ Module2/multithread_syn_scan.py
@@ -9,16 +9,17 @@ ip="192.168.0.1"
 
 class WorkerThread(threading.Thread):
 	
-	def __init__(self, queue):
+	def __init__(self, queue, outqueue):
 		threading.Thread.__init__(self)
 		self.queue = queue
+                self.outqueue = outqueue
 
         def scanner(self, ip, port):
 
             response = sr1(IP(dst=ip)/TCP(dport=port, flags="S"), verbose=False, timeout=.2)
             if response:
                 if response[TCP].flags == 18:
-                    print "[+] PortNumber %s OPEN"%port
+                    return "[+] PortNumber %s OPEN"%port
 
 	def run(self):
 	      #print "In WorkerThread"
@@ -26,19 +27,22 @@ class WorkerThread(threading.Thread):
 		step = 0
 		while True:
 			port = self.queue.get()
-                        self.scanner(ip, port)
+                        result = self.scanner(ip, port)
+                        if result !=None:
+                            self.outqueue.put(result)
                         self.queue.task_done()
 
 
 
 #queue to fetch work from 
 queue = Queue.Queue()
+outqueue = Queue.Queue()
 
 for i in range(10):
 	#print "Creating WorkerThread : %d" %i
 
 	#feed the queue to the Workerthread in order the threads know on what they 'll be working on
-	worker = WorkerThread(queue)
+	worker = WorkerThread(queue, outqueue)
 	worker.setDaemon(True)
 	worker.start()
 	#print "WorkerThread %d Created!"%i
@@ -49,5 +53,8 @@ for j in range(1024):
 
 queue.join()
 print "All ports scanned"
+
+for elem in list(outqueue.queue):
+    print elem
 
 
