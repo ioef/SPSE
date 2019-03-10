7e698c328789f47c45e0f77db23594158dd832b0
ioef <iosifidise@gmail.com>
2019-03-10 11:59:55 +0200

    Added text manipulation examples:

--- Module2/varlogs_exact.py
+++ Module2/varlogs_exact.py
@@ -3,13 +3,14 @@ import re
 import sys
 
 if len(sys.argv) !=2:
-    print "Wrong number of args. varlogs.py string"
+    print "Wrong number of args. varlog_exact.py string"
     sys.exit(1)
 
 word = sys.argv[1]
 
+
 with open("/var/log/messages", "r") as f:	
 	for line in f:
-		if line.find(word) !=-1 :
+		if re.search(r'\b%s\b'%word, line):
                     print line.strip()
 
